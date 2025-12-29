54686c8e4846372e10dc7cc52d3990d25c67d8ba
Brian King <brianjayking@gmail.com>
2025-12-29 15:46:37 -0600

    Complete Section 5 Lessons 45 - 46

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -53,16 +53,61 @@ const getDogPic = async () => {
         const data = await readFilePro(`${__dirname}/dog.txt`)
         console.log(`Breed: ${data}`) //Breed: schnauzer
 
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-        console.log(res.body.message) //https://images.dog.ceo/breeds/schnauzer/n02097209_1363.jpg
+        //const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        //console.log(res.body.message) //https://images.dog.ceo/breeds/schnauzer/n02097209_1363.jpg
+        //What if you wanted to wait for 3 images?
+        const res1Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        const res2Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        const res3Pro = superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
 
-        await writeFilePro('dog-img.txt', res.body.message)
+        const all = await Promise.all([res1Pro, res2Pro, res3Pro])
+        //console.log(all)
+        const imgs = all.map(el => el.body.message) //loop through all array and creates a new array (imgs) of with the body.message of each
+        const status = all.map(el => el.body.status)
+        console.log(imgs) //writes the array of 3 image links to the console
+        console.log(status)
+
+        //await writeFilePro('dog-img.txt', res.body.message)
+        await writeFilePro('dog-img.txt', imgs.join('\n')) //writes the 3 image links to the file with a newline between
         console.log('Random dog image saved to file!')
     } catch (err) {
         console.log(err)
+        //Execution of the current function will stop (the statements after throw won't be executed), 
+        // and control will be passed to the first catch block in the call stack. If no catch block 
+        // exists among caller functions, the program will terminate.
+        throw err
     }
+    return '2: READY!'
 }
-getDogPic()
+
+//IIFE - Immediately Invoked Function Expression
+(async () => {
+    try {
+        console.log('1: Will get dog pics!')
+        const x = await getDogPic() //the return of the promise is 2: READY!
+        console.log(x)
+        console.log('3: Done getting dog pics')
+    } catch(err) {
+        console.log('** ERROR! **')
+    }
+})()
+
+
+/*
+console.log('1: Will get dog pics!')
+//const x = getDogPic()
+//console.log(x) //Promise { <pending> } 
+//instead of writing to a variable, you can use the promise
+getDogPic().then(x => {
+    console.log(x)
+    console.log('3: Done getting dog pics')
+}).catch(err => {
+    console.log('** ERROR! **')
+})
+*/
+
+
+
 
 //using the readFilePro and writeFilePro functions
 //These still use Callback functions
