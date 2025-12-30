40752e2afcb83d8f10565e7e79af9a27d35deee1
Brian King <brianjayking@gmail.com>
2025-12-30 13:24:54 -0600

    Section 6 Lesson 51-52: Starting our API: Handling GET Requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////
 //Core Modules
 /////////////////////////////////////////////
-const express = require('express')
+const fs = require('fs')
 
 /////////////////////////////////////////////
 //Developer Modules
@@ -10,19 +10,43 @@ const express = require('express')
 /////////////////////////////////////////////
 //NPM Modules - 3rd Party
 /////////////////////////////////////////////
+const express = require('express')
 
+
+/////////////////////////////////////////////
+/// Top-Level Code - Only executed once on start up
+/////////////////////////////////////////////
+const app = express()
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 /////////////////////////////////////////////
 /// Routing
 /////////////////////////////////////////////
-const app = express()
-app.get('/', (req, res) => {
-    //res.status(200).send('Hello from the server side!')
-    res.status(200).json({message: 'Hello from the server side!', app: 'Natours'})
-})
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint...')
+// app.get('/', (req, res) => {
+//     //res.status(200).send('Hello from the server side!')
+//     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'})
+// })
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...')
+// })
+
+//it is good to specify the API version so that you can change the API in the future
+//the (res, req) => {} funtion is the Route Handler.
+//we want to send back all of the data for the tours. tours is the resource.
+//tours data comes from dev-data/tours-simple.json
+//in ES6 if the key and value have the same name you don't have to specify them. Can just write tours.
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            //tours: tours
+            tours
+        }
+    })
 })
 /////////////////////////////////////////////
 /// Web Server
