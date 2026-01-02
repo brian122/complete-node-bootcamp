4a54b9f50f8b1584ba0db881d41aa4c52a0be648
Brian King <brianjayking@gmail.com>
2026-01-02 08:59:57 -0600

    Section 6: Lesson 56: Handling DELETE Requsts

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -125,6 +125,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
         }
     })
 })
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    const id = Number(req.params.id)
+    if (id > tours.length) {
+        //console.log(Number(req.params.id))
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
 /////////////////////////////////////////////
 /// Web Server
 /////////////////////////////////////////////
