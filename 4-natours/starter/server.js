24801fd5c378d3fa755e3a6784647ae380298f67
Brian King <brianjayking@gmail.com>
2026-01-15 22:03:23 -0600

    Section 8 Lesson 83: Connect MongoDB to the Express App - needed to look into Q&A of course due to old version 5 instructions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,22 @@
+//mongoose is mongodb driver
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 
 dotenv.config({ path: './config.env' })
 //the environment varialbe needs to be before the app file
 const app = require('./app')
 
-
+//create a variable to hold our mongoDB connection string
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+//using mongoose connect method to connect to our mongoDB 
+//pass in our connection string from env files
+mongoose.connect(DB).then((con) => {
+  console.log(con.connection);
+  console.log('DB connection successfull!');
+});
 
 //Environment Variables
 console.log(app.get('env')) //set by express
