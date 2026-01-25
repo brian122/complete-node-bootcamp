5a35190433c6affcc6bccc64936600f0a6d874ac
Brian King <brianjayking@gmail.com>
2026-01-25 09:51:53 -0600

    Section 8 Lesson 90: Reading Documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -48,117 +48,141 @@ const Tour = require('../models/tourModel')
 //     next()
 // }
 
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime)
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     //tours: tours
-        //     tours
-        // }
-    })
+exports.getAllTours = async (req, res) => {
+	try {
+		// console.log(req.requestTime)
+		//.find returns an array of all of the documents converted into objects
+		const tours = await Tour.find()
+		res.status(200).json({
+			status: 'success',
+			// requestedAt: req.requestTime,
+			results: tours.length,
+			data: {
+				//tours: tours
+				tours,
+			},
+		})
+	} catch (err) {
+		res.status(404).json({
+			status: fail,
+			message: err,
+		})
+	}
 }
 
 //could have optional parameters using ?
 //app.get('/api/v1/tours/:id/:x?/:y?', (req, res)
 
-exports.getTour = (req, res) => {
-    console.log(req.params)
+exports.getTour = async (req, res) => {
+	try {
+		const tour = await Tour.findById(req.params.id)
 
-    const id = Number(req.params.id)
-    //const id = req.params.id *1  // can implicitly change variable type
+		res.status(200).json({
+			status: 'success',
+			data: {
+				tour,
+			},
+		})
+	} catch (err) {
+		res.status(404).json({
+			status: fail,
+			message: err,
+		})
+	}
+	// console.log(req.params)
 
-    // const tour = tours.find(el => el.id === id)
+	// const id = Number(req.params.id)
+	//const id = req.params.id *1  // can implicitly change variable type
 
-    // if (id > tours.length) {
-    // if (!tour) {
-    //     return res.status(404).json({
-    //         status: 'fail',
-    //         message: 'Invalid ID'
-    //     })
-    // }
-    // res.status(200).json({
-    //     status: 'success',
-    //     data: {
-    //         tour
-    //     }
-    // })
+	// const tour = tours.find(el => el.id === id)
+
+	// if (id > tours.length) {
+	// if (!tour) {
+	//     return res.status(404).json({
+	//         status: 'fail',
+	//         message: 'Invalid ID'
+	//     })
+	// }
+	// res.status(200).json({
+	//     status: 'success',
+	//     data: {
+	//         tour
+	//     }
+	// })
 }
 
 exports.createTour = async (req, res) => {
-    try {
-        //calls the create method on the Model
-        const newTour = await Tour.create(req.body)
-                
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
+	try {
+		//calls the create method on the Model
+		const newTour = await Tour.create(req.body)
+
+		res.status(201).json({
+			status: 'success',
+			data: {
+				tour: newTour,
+			},
+		})
+	} catch (err) {
+		res.status(400).json({
+			status: 'fail',
+			message: err,
+		})
+	}
 }
-    //console.log(req.body)
-    //the DB normally creates a new ID
-    //we will just +1 the last object in the json file
-    // const newId = tours[tours.length -1].id + 1
-    // //Object.assign creates a new object by merging two objects
-    // //could have req.body.id = newID but didn't want to mutate the original
-    // const newTour = Object.assign({ id: newId }, req.body)
-    // //push the new tour into the tours array
-    // tours.push(newTour)
-    // //persist the change to the file
-    // //We use the writeFile not the writeFileSync so we don't block the event loop
-    // //we have to stringify the tours object
-    // fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    //     //status of 201 = created
-    //     res.status(201).json({
-    //         status: 'success',
-    //         data: {
-    //             tour: newTour
-    //         }
-    //     })
-    // })
-    //since we are res with status we don't need this send
-    //res.send('Done')
+//console.log(req.body)
+//the DB normally creates a new ID
+//we will just +1 the last object in the json file
+// const newId = tours[tours.length -1].id + 1
+// //Object.assign creates a new object by merging two objects
+// //could have req.body.id = newID but didn't want to mutate the original
+// const newTour = Object.assign({ id: newId }, req.body)
+// //push the new tour into the tours array
+// tours.push(newTour)
+// //persist the change to the file
+// //We use the writeFile not the writeFileSync so we don't block the event loop
+// //we have to stringify the tours object
+// fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+//     //status of 201 = created
+//     res.status(201).json({
+//         status: 'success',
+//         data: {
+//             tour: newTour
+//         }
+//     })
+// })
+//since we are res with status we don't need this send
+//res.send('Done')
 // }
 
 exports.updateTour = (req, res) => {
-    // const id = Number(req.params.id)
-    // if (id > tours.length) {
-    //     //console.log(Number(req.params.id))
-    //     return res.status(404).json({
-    //         status: 'fail',
-    //         message: 'Invalid ID'
-    //     })
-    // }
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
+	// const id = Number(req.params.id)
+	// if (id > tours.length) {
+	//     //console.log(Number(req.params.id))
+	//     return res.status(404).json({
+	//         status: 'fail',
+	//         message: 'Invalid ID'
+	//     })
+	// }
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour: '<Updated tour here...>',
+		},
+	})
 }
 
 exports.deleteTour = (req, res) => {
-    //now handled by Param Middleware above
-    // const id = Number(req.params.id)
-    // if (id > tours.length) {
-    //     //console.log(Number(req.params.id))
-    //     return res.status(404).json({
-    //         status: 'fail',
-    //         message: 'Invalid ID'
-    //     })
-    // }
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+	//now handled by Param Middleware above
+	// const id = Number(req.params.id)
+	// if (id > tours.length) {
+	//     //console.log(Number(req.params.id))
+	//     return res.status(404).json({
+	//         status: 'fail',
+	//         message: 'Invalid ID'
+	//     })
+	// }
+	res.status(204).json({
+		status: 'success',
+		data: null,
+	})
 }
