e7b2c1fba9db4faa2e7e89d0807f97195d0e904b
Brian King <brianjayking@gmail.com>
2026-01-25 10:40:36 -0600

    Section 8 Lesson 92: Deleting Documents

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -188,7 +188,20 @@ exports.updateTour = async (req, res) => {
 	// }
 }
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
+	try {
+		await Tour.findByIdAndDelete(req.params.id)
+
+		res.status(204).json({
+			status: 'success',
+			data: null,
+		})
+	} catch (err) {
+		res.status(400).json({
+			status: 'fail',
+			message: err,
+		})
+	}
 	//now handled by Param Middleware above
 	// const id = Number(req.params.id)
 	// if (id > tours.length) {
@@ -198,8 +211,4 @@ exports.deleteTour = (req, res) => {
 	//         message: 'Invalid ID'
 	//     })
 	// }
-	res.status(204).json({
-		status: 'success',
-		data: null,
-	})
 }
